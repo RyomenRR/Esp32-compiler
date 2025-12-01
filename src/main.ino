862cdd3f0140c5b0a81506d8354196ce442d61ce
RyomenRR <164908607+RyomenRR@users.noreply.github.com>
2025-12-01 17:27:48 +0530

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,37 +1,71 @@
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include "RMaker.h"
+#include "WiFi.h"
+#include "BLEProvisioning.h"
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+// Relay pins
+#define RELAY1 23
+#define RELAY2 22
+#define RELAY3 19
+#define RELAY4 18
 
-// SPI wiring for SSD1306
-#define OLED_MOSI 23
-#define OLED_CLK 18
-#define OLED_DC 4
-#define OLED_CS 5
-#define OLED_RESET 2
+// Create RainMaker node
+static Node my_node("SwitchBoard", "SwitchBoardType");
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, OLED_DC, OLED_RESET, OLED_CS);
+// Device objects for each relay
+static PowerSwitch switch1("Relay1", NULL);
+static PowerSwitch switch2("Relay2", NULL);
+static PowerSwitch switch3("Relay3", NULL);
+static PowerSwitch switch4("Relay4", NULL);
+
+// Callback when any switch state is changed from the app
+void write_callback(Device *device, State &state) {
+    const char *devName = device->getDeviceName();
+    bool newState = state["power"].get<bool>();
+
+    if (!strcmp(devName, "Relay1")) digitalWrite(RELAY1, newState);
+    if (!strcmp(devName, "Relay2")) digitalWrite(RELAY2, newState);
+    if (!strcmp(devName, "Relay3")) digitalWrite(RELAY3, newState);
+    if (!strcmp(devName, "Relay4")) digitalWrite(RELAY4, newState);
+
+    device->updateAndReportParam(ESP_RMAKER_DEF_POWER_NAME, newState);
+}
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC)) {
-    Serial.println("OLED init failed");
-    for(;;);
-  }
+    // Relay pin setup
+    pinMode(RELAY1, OUTPUT);
+    pinMode(RELAY2, OUTPUT);
+    pinMode(RELAY3, OUTPUT);
+    pinMode(RELAY4, OUTPUT);
 
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
-  display.println("Hello ESP32 WROOM!");
-  display.println("SPI OLED Online");
-  display.display();
+    // Default OFF
+    digitalWrite(RELAY1, LOW);
+    digitalWrite(RELAY2, LOW);
+    digitalWrite(RELAY3, LOW);
+    digitalWrite(RELAY4, LOW);
+
+    // Assign callback
+    switch1.addCb(write_callback);
+    switch2.addCb(write_callback);
+    switch3.addCb(write_callback);
+    switch4.addCb(write_callback);
+
+    // Add devices to node
+    my_node.addDevice(switch1);
+    my_node.addDevice(switch2);
+    my_node.addDevice(switch3);
+    my_node.addDevice(switch4);
+
+    // RainMaker init (BLE only)
+    RMaker.initNode(my_node);
+
+    // Start BLE provisioning (local mode)
+    BLEProvisioning.begin("Relay-Board");
+
+    Serial.println("RainMaker BLE 4-Relay Board Ready!");
 }
 
 void loop() {
-  // Your logic here
+    // Nothing needed here; RainMaker handles everything
 }
